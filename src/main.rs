3dd4498e9db72190ddd6d3fbf397cdef3a857711
Jonathan Nakandala <jonathannakandala@gmail.com>
2020-05-03 17:46:45 +0100

    Draws incorrect picture to console

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,15 @@ fn main() {
     let squares_per_row = 8;
     let squares_per_col = 12;
 
-    let mut array: Vec<Vec<char>> =
-        vec![vec![Default::default(); console_dimensions[0]]; console_dimensions[1]];
-    let something = '\u{2813}';
-    print!("{}", something);
-
     fn draw_schotter(usizeimensions: i32, squares_per_row: i32, squares_per_col: i32) {
+        let console_dimensions = [66, 49];
         let canvas_width = usizeimensions * 2;
         let padding = if canvas_width > 4 { 2 } else { 0 };
         let square_side: f32 = (canvas_width - padding * 2) as f32 / squares_per_row as f32;
         let canvas_height = (square_side * squares_per_col as f32 * padding as f32 * 2.0) as i32;
         let rand_max = 2_147_483_647;
-        let mut canvas = vec![0; (canvas_width * canvas_width * canvas_height) as usize];
+        let mut canvas = vec![0; (canvas_width * canvas_height) as usize];
+        let mut output_canvas: Vec<Vec<char>> = vec![vec![Default::default(); 0]; 0];
         for y in 0..(squares_per_col - 1) {
             for x in 0..(squares_per_row - 1) {
                 let mut sx: i32 = x * square_side as i32 + square_side as i32 / 2 + padding;
@@ -59,10 +56,10 @@ fn main() {
                 draw_square(&mut canvas, sx, sy, square_side, angle, 1);
             }
         }
-        println!("{:?}", canvas);
+        //println!("{:?}", canvas);
+        render_canvas(&canvas, &mut output_canvas);
+        print_generated_braille(&output_canvas);
     }
-
-    fn create_canvas(canvas_width: i32, canvas_height: i32, color: i32) {}
 
     fn draw_square(
         mut canvas: &mut Vec<i32>,
@@ -90,7 +87,7 @@ fn main() {
         for j in 0..3 {
             let x2 = px[(j + 1) % 4];
             let y2 = py[(j + 1) % 4];
-            println!("x2: {}, y2: {}", x2, y2);
+            //println!("x2: {}, y2: {}", x2, y2);
             draw_line(&mut canvas, px[j], py[j], x2, y2, color);
         }
     }
@@ -132,46 +129,97 @@ fn main() {
         let canvas_height = 390;
 
         let index = x + (y * canvas_width);
-        println!(": {}", index);
+        //println!(": {}", index);
         if x < 0 || x >= canvas_width || y < 0 || y >= canvas_height {
-            println!("x: {}, Y {}", x, y);
-            canvas[(x + (y * canvas_width)) as usize] = color;
+            //println!("OR TRUE : x: {}, Y {}", x, y);
+            return;
+        }
+        //println!("x: {}, Y {}", x, y);
+        canvas[(x + (y * canvas_width)) as usize] = color
+    }
+
+    fn get_pixel(canvas: &Vec<i32>, x: i32, y: i32) -> i32 {
+        let canvas_width = 66 * 2;
+
+        if x < 0 || x >= canvas_width || y < 0 || y >= canvas_width {
+            return 0;
+        }
+        return canvas[(x + (y * canvas_width)) as usize];
+    }
+
+    fn integer_is_one(value: i32) -> bool {
+        value == 1
+    }
+
+    fn render_canvas(canvas: &Vec<i32>, output_canvas: &mut Vec<Vec<char>>) {
+        let canvas_width = 66 * 2;
+        let canvas_height = 390;
+
+        for y in (0..canvas_height - 1).step_by(4) {
+            let mut braille_row: Vec<char> = Vec::new();
+            for x in (0..canvas_width - 1).step_by(2) {
+                let mut byte: u8 = 0;
+
+                if integer_is_one(get_pixel(canvas, x, y)) {
+                    byte |= 1 << 0
+                };
+                if integer_is_one(get_pixel(canvas, x, y + 1)) {
+                    byte |= 1 << 1
+                };
+                if integer_is_one(get_pixel(canvas, x, y + 2)) {
+                    byte |= 1 << 2
+                };
+                if integer_is_one(get_pixel(canvas, x + 1, y)) {
+                    byte |= 1 << 3
+                };
+                if integer_is_one(get_pixel(canvas, x + 1, y + 1)) {
+                    byte |= 1 << 4
+                };
+                if integer_is_one(get_pixel(canvas, x + 1, y + 2)) {
+                    byte |= 1 << 5
+                };
+                if integer_is_one(get_pixel(canvas, x, y + 3)) {
+                    byte |= 1 << 6
+                };
+                if integer_is_one(get_pixel(canvas, x, y + 3)) {
+                    byte |= 1 << 7
+                };
+                translate_to_braille(byte, &mut braille_row);
+            }
+            //println!("Here's the row!!!!");
+            //println!("{:?}", braille_row);
+            output_canvas.push(braille_row);
         }
     }
 
-    fn pixel_to_braille(byte: u8) -> i32 {
-        2
+    fn translate_to_braille(byte: u8, braille_row: &mut Vec<char>) {
+        let code: u32 = 0x2800 + byte as u32;
+        let unicode_char = from_u32(code as u32).unwrap();
+        //print!("{}", unicode_char);
+        braille_row.push(unicode_char);
     }
 
-    let mut cnt = 2813;
-    for row in &mut array {
-        // row: &mut [u8; 3]
-        for col in row {
-            // col: &mut u8
-            cnt += 1;
-            let character = from_u32(0x2800 + 4).unwrap();
-            *col = character;
+    fn print_generated_braille(output_canvas: &Vec<Vec<char>>) {
+        let console_dimensions = [66, 49];
+        let mut print_row: Vec<&[char]> = Vec::new();
+        for row in output_canvas {
+            print_row.push(row);
         }
-    }
 
-    let mut print_row: Vec<&[char]> = Vec::new();
-    for row in &array {
-        print_row.push(row);
-    }
-
-    let slice: &[&[char]] = &print_row;
-    for row in slice {
-        for col in *row {
-            print!("{}", col)
+        let slice: &[&[char]] = &print_row;
+        for row in slice {
+            for col in *row {
+                print!("{}", col)
+            }
+            println!();
         }
-        println!();
-    }
 
-    println!(
-        "Width: {} Height: {}",
-        console_dimensions[0], console_dimensions[1]
-    );
-    println!("\nGeorg Nees - schotter, plotter on paper, 1968. Jono ver. ");
+        println!(
+            "Width: {} Height: {}",
+            console_dimensions[0], console_dimensions[1]
+        );
+        println!("\nGeorg Nees - schotter, plotter on paper, 1968. Jono ver. ");
+    }
 
     draw_schotter(
         console_dimensions[0] as i32,
